77ab6844ace5755cbde12aca0f9d87e94b2b940a
Janelle Johnson <janelleblevins@gmail.com>
2020-10-15 16:29:32 -0500

    Finished edit

--- Superheros/Controllers/SuperherosController.cs
+++ Superheros/Controllers/SuperherosController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -68,6 +69,28 @@ namespace Superheros.Controllers
         {
             try
             {
+                //identify the area the superhero has been updated on the form
+                // update the property that has been selected
+                var superheroToUpdate = db.Superheros.Where(s => s.Id == id).SingleOrDefault();
+
+                if (superhero.SuperheroName != null)
+                {
+                    superheroToUpdate.SuperheroName = superhero.SuperheroName;
+                }
+                if (superhero.PrimarySuperAbility != null)
+                {
+                    superheroToUpdate.PrimarySuperAbility = superhero.PrimarySuperAbility;
+                }
+                if (superhero.SecondarySuperAbility != null)
+                {
+                    superheroToUpdate.SecondarySuperAbility = superhero.SecondarySuperAbility;
+                }
+                if (superhero.Catchphrase != null)
+                {
+                    superheroToUpdate.Catchphrase = superhero.Catchphrase;
+                }
+                db.SaveChanges();
+
                 return RedirectToAction(nameof(Index));
             }
             catch
--- Superheros/Models/Superhero.cs
+++ Superheros/Models/Superhero.cs
@@ -9,10 +9,16 @@ namespace Superheros.Models
     public class Superhero
     {
         [Key]
+        public int Id { get; set; }
+        [Display (Name = "Superhero Name")]
         public string SuperheroName { get; set; }
+        [Display (Name ="Alter Ego Name")]
         public string AlterEgoName { get; set; }
+        [Display (Name = "Primary Super Ability" )]
         public string PrimarySuperAbility { get; set; }
+        [Display (Name = "Secondary Super Ability")]
         public string SecondarySuperAbility { get; set; }
+        [Display (Name = "Catch Phrase")]
         public string Catchphrase { get; set; }
     }
 }
